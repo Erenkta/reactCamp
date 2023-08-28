0a67fa4aafbf492fb81c515020e479ef9d5c4c2e
Eren Nokta <eren.nkta@gmail.com>
2023-08-28 12:56:01 +0300

    React Camp Props Commited Bu kısımda Navi Component'imizin içine 2 yeni component ekledik. Ve Navi -> SignedIn , Navi->SignedOut arasında state geçisini sağladık. Bu state geçişini props adını verdiğimiz objeler ile kolayca sağladık. Ve Navi'nin alt componentlerinden Navi'ye ait olan bir state'in değişmesini ele aldık

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,23 +1,27 @@
-import React from "react";
-import '../App.css';
-import { Button, Container, Menu } from "semantic-ui-react";
-import CartSummary from  "./CartSummary"
+import React, { useState } from "react";
+import "../App.css";
+import {Container, Menu } from "semantic-ui-react";
+import CartSummary from "./CartSummary";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
 
 export default function Navi() {
+  const [isAuthenticated,setIsAuthenticated] = useState(false)
+
+  function handleSign() {
+    setIsAuthenticated(!isAuthenticated) //Bunu kendim düşündüm
+  }
+  
   return (
     <div className="App">
-      <Menu inverted  size="huge" fixed="top">  {/*inverted rengini siyah temaya çevirdi , oraya fixed yazsaydık sayfayı kaydırınca top bar bizle beraber aşağı gelirdi*/}
-          <Container className="navileft">
+      <Menu inverted size="huge" fixed="top">
+        {/*inverted rengini siyah temaya çevirdi , oraya fixed yazsaydık sayfayı kaydırınca top bar bizle beraber aşağı gelirdi*/}
+        <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
-          </Container>
-
-          <Container>
           <Menu.Menu position="right">
             <CartSummary />
-            <Menu.Item >
-              <Button primary>Sign Up</Button>
-            </Menu.Item>
+            {isAuthenticated ? <SignedIn signOut = {handleSign}/> : <SignedOut signIn={handleSign}/>} {/*Alt component'e data gecmek signOut = {handleSignOut} bunlara props denir! */ }
           </Menu.Menu>
         </Container>
       </Menu>
